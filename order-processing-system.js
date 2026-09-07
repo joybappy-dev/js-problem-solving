c819905748284ae6e2f86ad8ec080f17a7de9640
Joy Bappy <joybappy2025@gmail.com>
2026-09-07 20:57:49 +0600

    create order processing system

---
+++ order-processing-system.js
@@ -0,0 +1,26 @@
+// function isValidEmail(email) {
+//     if (email.includes(".") && email.includes("@")) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// function checkPrice(price) {
+    
+// }
+// const pr = checkPrice(0);
+// console.log(pr);
+
+// function processOrder(user) {
+//     if (!isValidEmail(user.email)) {
+//         console.log("Error: Invalid email address!");
+//     }
+//     console.log("Order placed successfully.");
+// }
+
+// const user1 = {
+//     name: "Joy",
+//     email: "joybappy@yahoo.com",
+// };
+// // processOrder(user1);
