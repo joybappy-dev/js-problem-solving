fbb0dbb967a7cd508bcf8c4c6970529feddbfb98
Joy Bappy <joybappy2025@gmail.com>
2026-09-08 08:49:56 +0600

    update order-processing-system

--- order-processing-system.js
+++ order-processing-system.js
@@ -1,26 +1,58 @@
-// function isValidEmail(email) {
-//     if (email.includes(".") && email.includes("@")) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
+function checkEmail(email) {
+    return email.includes(".") && email.includes("@");
+}
 
-// function checkPrice(price) {
-    
-// }
-// const pr = checkPrice(0);
-// console.log(pr);
+function checkPrice(price) {
+    return price > 0 && typeof price === "number";
+}
 
-// function processOrder(user) {
-//     if (!isValidEmail(user.email)) {
-//         console.log("Error: Invalid email address!");
-//     }
-//     console.log("Order placed successfully.");
-// }
+function applyDiscount(price, percentageOfDiscount, cuponCode) {
+    if (cuponCode === "hello") {
+        let discountAmont = (price * percentageOfDiscount) / 100;
+        let priceAfterDiscount = price - discountAmont;
+        return priceAfterDiscount;
+    }
+}
 
-// const user1 = {
-//     name: "Joy",
-//     email: "joybappy@yahoo.com",
-// };
-// // processOrder(user1);
+function prepareBill(finalPrice, user) {
+    console.log(`Hi! Mr. ${user.name}\nYour Orderplaced Successfully.`);
+    console.log(`customer: ${user.name}\nEmail: ${user.email}`);
+    console.log(`Total Price: ${finalPrice}`);
+    console.log(`Order placed at: ${new Date().toLocaleString()}`);
+    console.log("Thank you for your order.");
+}
+
+function processOrder(user, price, cuponCode) {
+    const isValidEmail = checkEmail(user.email);
+    if (!isValidEmail) {
+        console.log("Invalid user!");
+        return;
+    }
+
+    const isValidPrice = checkPrice(price);
+    if (!isValidPrice) {
+        console.log("Invalid Price");
+        return;
+    }
+
+    let finalPrice = price;
+    if (cuponCode) {
+        const priceAfterDiscount = applyDiscount(price, 10, "hello");
+        finalPrice = priceAfterDiscount;
+    }
+
+    prepareBill(finalPrice, user);
+}
+
+const user1 = {
+    name: "Joy",
+    email: "joy@g.com",
+};
+const user2 = {
+    name: "Rahim",
+    email: "rahim@c.com",
+};
+
+processOrder(user1, 100, "hello");
+processOrder(user2, 600, "hello");
+processOrder({ name: "Unknown", email: "unknown@u.com" }, 300);
