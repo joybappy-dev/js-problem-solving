2341a43c9b4fb4b06ef51f6cc4bcfe82a1ccd85b
Joy Bappy <joybappy2025@gmail.com>
2026-09-10 09:58:04 +0600

    rest operator to take unlimited args

---
+++ rest.js
@@ -0,0 +1,33 @@
+function addNumbers(...numbers) {
+    let sum = 0;
+    for (let n of numbers) {
+        sum += n;
+    }
+    return sum;
+}
+
+const sum = addNumbers(
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+);
+console.log(sum);
+
+const arr = [1,2,3]
