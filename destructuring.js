110b674818da06b772762532bf9c5bacbd4b3a87
Joy Bappy <joybappy2025@gmail.com>
2026-09-10 08:12:58 +0600

    destructuring array

--- destructuring.js
+++ destructuring.js
@@ -1,12 +1,18 @@
+// destructuring object
 const person = {
-    name: 'Titenium',
+    name: "Titenium",
     position: 166,
     address: {
-        area: 'Hisham',
-        road: '82/7'
-    }
-}
+        area: "Hisham",
+        road: "82/7",
+    },
+};
 
-const {name, address:myAddress} = person;
-const {area, road} = myAddress;
-console.log(myAddress)
+const { name, address: myAddress } = person;
+const { area, road } = myAddress;
+// console.log(myAddress)
+
+// desturing array
+const colors = ["RED", "GREEN", "BLACK"];
+const [,,kalo] = colors;
+// console.log(kalo)
